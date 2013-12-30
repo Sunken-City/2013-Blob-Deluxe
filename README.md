0db355f63d580cb9560f38a91c286d9a89a7a811
picoriley <acloudy@mail.smu.edu>
2013-12-29 21:07:15 -0600

    Working on the genetics engine outside of unity until I can figure out where everything needs to go

--- README.md
+++
@@ -1,4 +0,0 @@
-2013-Blob-Deluxe
-================
-
-An attempt at genetics and Unity 2D
---
+++ VS Mockups/Gengine/Gengine/Gene.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gengine
+{
+    public class Gene <T>
+    {
+        public Allele<T> alleleA;
+        public Allele<T> alleleB;
+
+        public Gene(Allele<T> input1, Allele<T> input2)
+        {
+            alleleA = input1;
+            alleleB = input2;
+        }
+
+        public Gene(T input1, T input2)
+        {
+            alleleA = new Allele<T> (input1);
+            alleleB = new Allele<T> (input2);
+        }
+
+        public Gene(T input1, int dom1, T input2, int dom2)
+        {
+            alleleA = new Allele<T>(input1, dom1);
+            alleleB = new Allele<T>(input2, dom2);
+        }
+    }
+}
