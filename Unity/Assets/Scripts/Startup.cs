e00ded43f59a2e37097f6a33d8628b7f61f09264
picoriley <acloudy@mail.smu.edu>
2014-01-04 21:07:28 -0600

    FINALLY FIGURED OUT HOW TO CREATE OBJECTS AT RUNTIME/! This has been a huge roadblock for me learning unity, that and the weird heirarchy

---
+++ Unity/Assets/Scripts/Startup.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Startup : MonoBehaviour {
+
+	public GameObject mom;
+	public GameObject dad;
+	// Use this for initialization
+	void Start () 
+	{
+		mom = (GameObject)Instantiate(Resources.Load("Blob"));
+		dad = (GameObject)Instantiate(Resources.Load("Blob"));
+		
+		mom.transform.position = new Vector2(3, transform.position.y);
+		dad.transform.position = new Vector2(-3, transform.position.y);
+		
+		mom.transform.eulerAngles = new Vector2(0, 180);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
