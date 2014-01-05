674d6db65ab4e140286ee9ae9274e57ef6b54f63
picoriley <acloudy@mail.smu.edu>
2014-01-04 20:24:20 -0600

    I may have finally gotten the classes to work! They compile and are part of a game object

---
+++ Unity/Assets/Scripts/Blob.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Linq;
+using Gengine;
+
+public class Blob : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	public enum Gender { Male, Female }
+	public int gender;
+	public int numGenes = 0;
+	
+	private SortedList DNA = new SortedList();
+	
+	
+	public Blob(Gene<char> input)
+	{
+		addGene<char>(input);
+	}
+	
+	public Blob()
+	{
+		gender = 1;
+	}
+	
+	public void addGene<T>(Gene<T> gene)
+	{
+		DNA.Add(gene.geneName, gene);
+		numGenes++;
+	}
+	
+	public Gene<T> getGene<T>(string name)
+	{
+		return DNA[name] as Gene<T>;
+	}
+}
---
+++ Unity/Assets/Scripts/Punnett.cs
@@ -0,0 +1,115 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+
+namespace Gengine
+{
+	public class Punnett : MonoBehaviour
+	{
+	
+        public static System.Random random = new System.Random();
+
+        public static Gene<T> cross<T>(Gene<T> g1, Gene<T> g2)
+        {
+            string name;
+			if (g1.geneName != g2.geneName)
+				return null; //Error: Names don't match
+            else
+				name = g1.geneName;
+
+			int randomNumber = random.Next(1, 5);
+            Gene<T> offspring;
+
+            if (randomNumber == 1)
+                if (g1.alleleA.dominance >= g2.alleleA.dominance)
+                    offspring = new Gene<T>(name, g1.alleleA, g2.alleleA);
+                else
+                    offspring = new Gene<T>(name, g2.alleleA, g1.alleleA);
+
+            else if (randomNumber == 2)
+                if (g1.alleleA.dominance >= g2.alleleB.dominance)
+                    offspring = new Gene<T>(name, g1.alleleA, g2.alleleB);
+                else
+                    offspring = new Gene<T>(name, g2.alleleB, g1.alleleA);
+
+            else if (randomNumber == 3)
+                if (g1.alleleB.dominance >= g2.alleleA.dominance)
+                    offspring = new Gene<T>(name, g1.alleleB, g2.alleleA);
+                else
+                    offspring = new Gene<T>(name, g2.alleleA, g1.alleleB);
+
+            else
+                if (g1.alleleB.dominance >= g2.alleleB.dominance)
+                    offspring = new Gene<T>(name, g1.alleleB, g2.alleleB);
+                else
+                    offspring = new Gene<T>(name, g2.alleleB, g1.alleleB);
+
+            return offspring;
+        }
+
+        public static Blob cross(Blob mom, Blob dad)
+        {
+
+            Blob offspring = new Blob();
+
+            if (mom.numGenes == dad.numGenes)
+            {
+                offspring.addGene<char>(cross<char>(mom.getGene<char>("Example"), dad.getGene<char>("Example")));
+                return offspring;
+            }
+
+            else
+            {
+                return null;
+            }
+        }
+    }
+    
+	public class Gene<T>
+	{
+		public Allele<T> alleleA;
+		public Allele<T> alleleB;
+		public string geneName;
+		
+		public Gene(string n, Allele<T> input1, Allele<T> input2)
+		{
+			geneName = n;
+			alleleA = input1;
+			alleleB = input2;
+		}
+		
+		public Gene (string n, T input1, T input2)
+		{
+			geneName = n;
+			this.alleleA = new Allele<T> (input1);
+			this.alleleB = new Allele<T> (input2);
+		}
+		
+		public Gene(string n, T input1, int dom1, T input2, int dom2)
+		{
+			geneName = n;
+			this.alleleA = new Allele<T>(input1, dom1);
+			this.alleleB = new Allele<T>(input2, dom2);
+		}
+	}
+	
+	public class Allele<T> 
+	{
+		public T value;
+		public int dominance;
+		
+		public Allele(T input)
+		{
+			value = input;
+			dominance = 0;
+		}
+		
+		public Allele(T input, int dom)
+		{
+			value = input;
+			dominance = dom;
+		}
+	}
+}
