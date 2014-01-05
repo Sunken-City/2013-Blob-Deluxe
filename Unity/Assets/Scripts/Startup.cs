47b6b36eebb37b9c23093102e57f3527fd94e8f2
picoriley <acloudy@mail.smu.edu>
2014-01-05 14:56:50 -0600

    Forgot a few files in the last commit

--- Unity/Assets/Scripts/Startup.cs
+++ Unity/Assets/Scripts/Startup.cs
@@ -7,6 +7,8 @@ public class Startup : MonoBehaviour {
 	public GameObject mom;
 	public GameObject dad;
 	public GameObject child;
+	public GameObject GO;
+	public float distance = -3.0f;
 	// Use this for initialization
 	void Start () 
 	{
@@ -17,17 +19,24 @@ public class Startup : MonoBehaviour {
 		mom.transform.position = new Vector2(3, transform.position.y);
 		dad.transform.position = new Vector2(-3, transform.position.y);
 		
+		//Add markings to the dad
+		//GO = (GameObject)Instantiate(Resources.Load("dogmarkings"));
+		//GO.transform.parent = dad.transform;
+		//GO.transform.position = new Vector3(-3, transform.position.y, -1);
+		
 		//Flip the Mommy around so that she faces the Daddy
 		mom.transform.eulerAngles = new Vector2(0, 180);
 		
 		//Make the Mom red and the Dad green
-		LABColor c1 = new LABColor(Color.black);
-		LABColor c2 = new LABColor(Color.green);
+		LABColor c1 = new LABColor(Color.yellow);
+		LABColor c2 = new LABColor(Color.red);
+		LABColor c3 = new LABColor(Color.white);
+		LABColor c4 = new LABColor(Color.black);
 		mom.GetComponent<SpriteRenderer>().color = c1.ToColor();
 		dad.GetComponent<SpriteRenderer>().color = c2.ToColor();
 		
-		mom.GetComponent<Blob>().addGene<LABColor>("Color", c1, c1);
-		dad.GetComponent<Blob>().addGene<LABColor>("Color", c2, c2);
+		mom.GetComponent<Blob>().addGene<LABColor>("Color", c1, c3);
+		dad.GetComponent<Blob>().addGene<LABColor>("Color", c2, c4);
 		
 		child = Punnett.cross(mom.GetComponent<Blob>(), dad.GetComponent<Blob>());
 		child.transform.position = new Vector2(0, transform.position.y);
@@ -37,7 +46,9 @@ public class Startup : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update () 
+	{
+		//distance += 0.1f;
+		//dad.transform.position = new Vector2(distance , transform.position.y);
 	}
 }
